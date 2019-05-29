69840be53ed8677fc70032fcd64f21d4915f4ea6
watanabe-mf <watanabe.hiroto.702@moneyforward.co.jp>
2019-05-29 13:30:56 +0900

    ローディングUI修正

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -5,10 +5,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const Loading = props => {
   return(
     <Root isLoading={props.isLoading}>
-      <Box>
-        <Icon icon="circle-notch" spin />
-        <Text>{ props.text }</Text>
-      </Box>
+      <Icon icon="circle-notch" spin />
+      <Text>{ props.text }</Text>
     </Root>
   );
 }
@@ -25,6 +23,7 @@ const Root = styled.div`
   top: 0;
   left: 0;
   display: ${props => props.isLoading ? 'flex' : 'none'};
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   background-color: rgba(0, 0, 0, 0.5);
@@ -32,18 +31,13 @@ const Root = styled.div`
   height: 100vh;
 `
 
-const Box = styled.div`
-  padding: 50px;
-  border-radius: 5px;
-  background-color: #fff;
-`
-
 const Icon = styled(FontAwesomeIcon)`
   margin-bottom: 20px;
-  font-size: 8rem;
+  font-size: 5rem;
+  color: #fff;
 `
 
 const Text = styled.p`
-  text-align: center;
   font-size: 2rem;
+  color: #fff;
 `
